50dceec4298aec4ac0e1b46832a91baca2c1ed3c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-08 14:40:03 +0900

    feat: Implement use to let variable in script.js

--- var, let, const/script.js
+++ var, let, const/script.js
@@ -1,4 +1,4 @@
-// var
+// var: 중복 선언과 재할당 가능
 var greeting = 'hello';
 console.log(greeting);
 
@@ -6,4 +6,15 @@ var greeting = 'hi';
 console.log(greeting);
 
 greeting = 'how are you?';
+console.log(greeting);
+
+// let: 중복 선언 불가, 재할당 가능
+let greet = 'hello';
+console.log(greeting);
+
+// 다음과 같이 선언할 경우 Error가 발생
+// let greet = 'hi';
+// console.log(greeting);
+
+greet = 'hi';
 console.log(greeting);
