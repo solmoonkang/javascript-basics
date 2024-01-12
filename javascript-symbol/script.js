7f8df16b3842111674ccb698c462e11493fe64a0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-12 15:11:20 +0900

    feat: Implement symbol using case in script.js

--- javascript-symbol/script.js
+++ javascript-symbol/script.js
@@ -61,3 +61,149 @@ let sym5 = Symbol.for("id");
 // 심볼을 이용해 description을 얻음
 alert(Symbol.keyFor(sym4)); // name
 alert(Symbol.keyFor(sym5)); // id
+
+
+
+
+
+
+
+// Use Case
+// 아래의 내용을 Case를 사용해서 구현할 수 있다.
+// const RED = 'red';
+// const ORANGE = 'orange';
+// const YELLOW = 'yellow';
+// const BLUE = 'blue';
+// const dog = 'blue';
+
+// function getImportantLevel(color){
+//     switch (color) {
+//         case RED:
+//             return 'very important';
+//         case ORANGE:
+//             return 'important';
+//         case YELLOW:
+//             return 'little important';
+//         case BLUE:
+//             return 'not important';
+//         default:
+//             console.log(`${color} not included`);
+//     }
+// }
+// console.log(getImportantLevel(BLUE));
+// console.log(getImportantLevel(dog));
+
+
+
+// 위와 같이 작성할 경우, 실수로 console.log(getImportantLevel(dog)); 다음과 같이 출력해도 blue라는 값이 출력되게 된다.
+// 이를 방지하기 위해 Symbol을 이용해서 다음과 같이 구현할 수 있다.
+const RED = Symbol('red');
+const ORANGE = Symbol('orange');
+const YELLOW = Symbol('yellow');
+const BLUE = Symbol('blue');
+const dog = 'blue';
+
+function getImportantLevel(color) {
+    switch (color) {
+        case RED:
+            return 'very important';
+        case ORANGE:
+            return 'important';
+        case YELLOW:
+            return 'little important';
+        case BLUE:
+            return 'not important';
+        default:
+            console.log(`${color} not included`);
+    }
+}
+console.log(getImportantLevel(BLUE));
+console.log(getImportantLevel(dog));
+
+
+
+
+// class Car {
+//     constructor() {
+//         this.length = 0;
+//     }
+
+//     add(brand, name) {
+//         this[brand] = name;
+//         this.length++;
+//     }
+// }
+
+// let myCars = new Car();
+// myCars.add('kia', 'morning');
+// myCars.add('hyundai', 'tucson');
+// myCars.add('genesis', 'gv80');
+
+// for (const car in myCars) {
+//     console.log(car, myCars[car]);
+// }
+
+const length = Symbol('length');
+class Car {
+    constructor() {
+        this[length] = 0;
+    }
+
+    add(brand, name) {
+        this[brand] = name;
+        this[length]++;
+    }
+}
+
+let myCars = new Car();
+myCars.add('kia', 'morning');
+myCars.add('hyundai', 'tucson');
+myCars.add('genesis', 'gv80');
+
+for (const car in myCars) {
+    console.log(car, myCars[car]);
+}
+
+
+
+
+
+class AlertService {
+    constructor() {
+        this.alerts = {};
+    }
+
+    addAlert(symbol, alertText) {
+        this.alerts[symbol] = alertText;
+        this.renderAlerts();
+    }
+
+    removeAlert(symbol) {
+        delete this.alerts[symbol];
+    }
+
+    renderAlerts() { }
+}
+
+const alertService = new AlertService();
+
+class MyComponent {
+    constructor(thing) {
+        this.componentId = Symbol(thing);
+    }
+
+    errorHandler(msg) {
+        alertService.addAlert(this.componentId, msg);
+        setTimeout(() => {
+            alertService.removeAlert(this.componentId);
+            console.log('Removed alert', this.componentId);
+        }, 5000);
+    }
+}
+
+let list = new MyComponent('listComponent');
+let list2 = new MyComponent('listComponent');
+let form = new MyComponent('inputComponent');
+
+list.errorHandler('문제가 발생했습니다.');
+list.errorHandler('이러이러한 이유로 에러가 발생했습니다.');
