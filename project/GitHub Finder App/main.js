55bfc77862e127be3b66088ee806b73e46ec4d05
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-15 16:49:17 +0900

    feat: Implement search username keypress event in main.js

---
+++ project/GitHub Finder App/main.js
@@ -0,0 +1,11 @@
+// main.js
+import GithubFinder from './github-finder.js';
+
+const finder = new GithubFinder();
+
+document.querySelector('.search-bar').addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        finder.searchUser();
+        finder.fetchRepos();
+    }
+});
